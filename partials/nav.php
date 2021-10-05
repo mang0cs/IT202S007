e1d70794f24fa26a6afd1591b0aefec1dbf0c1dd
mang0cs <era3@njit.edu>
2021-10-05 11:03:47 -0400

    finished navigation

--- partials/nav.php
+++ partials/nav.php
@@ -1,28 +1,34 @@
+
 <?php
-//include functions here so we can have it on every page that uses the nav bar
-//that way we don't need to include so many other files on each page
-//nav will pull in functions and functions will pull in db
+//Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
-if(strpos($domain, ":"))
-{
+if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
 session_set_cookie_params([
     "lifetime" => 60 * 60,
     "path" => "/Project",
+    //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
     "domain" => $domain,
-    //domain" => $_SERVER["HTTP_HOST"] || "localhost",
     "secure" => true,
     "httponly" => true,
-    "samesite" => "lax" 
+    "samesite" => "lax"
 ]);
 session_start();
-require(__DIR__."/../lib/functions.php");
+require_once(__DIR__ . "/../lib/functions.php");
+
 ?>
 <nav>
     <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="logout.php">Logout</a></li>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="home.php">Home</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -32,9 +32,9 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         array_push($errors, "Email must be set");
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
     //validate
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!is_valid_email($email)) {
         array_push($errors, "Invalid email address");
     }
     if (empty($password)) {
@@ -59,6 +59,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     if (password_verify($password, $hash)) {
                         echo "Welcome $email";
                         $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid password";
                     }
